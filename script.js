c04b6ffa60e3a1f01ee100bbb7d36fa820ed2fdf
Danilonbs <danilo_eu_sk8@hotmail.com>
2023-03-22 20:22:49 -0300

    cart add

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 
 const c = (el) => document.querySelector(el);
@@ -18,6 +20,7 @@ pizzaJson.map((item, index) => {
 
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -55,4 +58,47 @@ function closeModal() {
 
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
-} )
+} );
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+    
+})
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    if (modalQt < 10) {
+        modalQt++;
+        c('.pizzaInfo--qt').innerHTML == modalQt;
+    }
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+   size.addEventListener('click', (e) => {
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected')
+   })
+});
+c('.pizzaInfo--addButton').addEventListener('click', () => {
+   let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+   
+    let indentifier = pizzaJson[modalKey].id+'@'+size;
+
+    let key = cart.findIndex((item) => item.indentifier = indentifier);
+
+if(key > -1) {
+    cart[key].qt += modalQt;
+}else {
+    cart.push({
+        indentifier,
+        id:pizzaJson[modalKey].id,
+        size,
+        qt:modalQt
+       });
+}
+
+   
+
+   closeModal();
+});
